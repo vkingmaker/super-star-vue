42f50a33a36de9c1e15fa9b72bc45804bc9d99ad
Monday Victor A <vkingmaker1@gmail.com>
2019-10-31 23:04:37 +0100

    add landing page
    this commit adds the landing page with dummy navigation item that will
    be releveant to the full blown application
    
    #Deliver [landingPage]

--- src/App.vue
+++ src/App.vue
@@ -1,45 +1,73 @@
 <template>
-<mdb-container>
-  <mdb-row id="app">
-    <mdb-col id="nav" md="12">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </mdb-col>
-    <router-view class="mx-auto"/>
+ <mdb-container class="main-content" fluid>
+  <mdb-row>
+  <mdb-col class="w-100 px-0">
+  <mdb-navbar expand="large" dark color="black">
+    <mdb-navbar-brand href="#">
+      Lee-Nation
+    </mdb-navbar-brand>
+    <mdb-navbar-toggler>
+      <mdb-navbar-nav right>
+        <mdb-nav-item href="#" active>Dashboard</mdb-nav-item>
+        <mdb-nav-item href="#">Musics</mdb-nav-item>
+        <mdb-nav-item href="#">Pictures</mdb-nav-item>
+        <mdb-nav-item href="#">Videos</mdb-nav-item>
+        <mdb-nav-item href="#">Tours</mdb-nav-item>
+        <mdb-nav-item href="#">Register</mdb-nav-item>
+        <mdb-nav-item href="#">Login</mdb-nav-item>
+        <mdb-dropdown tag="li" class="nav-item">
+          <mdb-dropdown-toggle tag="a" navLink color="transparent" slot="toggle" waves-fixed>Dropdown</mdb-dropdown-toggle>
+          <mdb-dropdown-menu>
+            <mdb-dropdown-item>Logout</mdb-dropdown-item>
+          </mdb-dropdown-menu>
+        </mdb-dropdown>
+      </mdb-navbar-nav>
+    </mdb-navbar-toggler>
+  </mdb-navbar>
+   <router-view />
+  </mdb-col>
   </mdb-row>
-</mdb-container>
+   </mdb-container>
+
 </template>
 
 <script>
-import { mdbContainer, mdbRow, mdbCol } from "mdbvue";
+import {
+  mdbDropdown,
+  mdbDropdownToggle,
+  mdbDropdownMenu,
+  mdbDropdownItem,
+  mdbNavbar,
+  mdbContainer,
+  mdbRow,
+  mdbCol,
+  mdbNavbarBrand,
+  mdbNavbarToggler,
+  mdbNavbarNav,
+  mdbNavItem
+} from "mdbvue";
 export default {
+  name: "HelloWorld",
   components: {
+    mdbNavbar,
+    mdbNavbarBrand,
+    mdbNavbarToggler,
+    mdbNavbarNav,
+    mdbNavItem,
+    mdbDropdown,
     mdbContainer,
     mdbRow,
-    mdbCol
+    mdbCol,
+    mdbDropdownToggle,
+    mdbDropdownMenu,
+    mdbDropdownItem
   }
 };
 </script>
-
 <style>
-#app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
+.main-content {
+  max-width: 100vw;
+  height: 100vh;
+  overflow: hidden;
 }
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,40 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+<div>
+  <img src="https://www.patchwerk.com/images/home/new-client-slide.jpg" alt="" class="w-100">
+  <div class="welcome-msg">
+  <h2 class="display-4">Oliver twist</h2>
+<p class="lead">Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque iusto itaque pariatur dolore explicabo iure perferendis velit odit beatae quam quia nesciunt debitis quis totam, tempora sit, alias in provident!</p>
+<mdb-btn outline="danger" darkWaves rounded>Stream on all platforms</mdb-btn>
   </div>
+</div>
 </template>
-
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
+import { mdbBtn } from "mdbvue";
 export default {
-  name: "home",
+  name: "ButtonPage",
   components: {
-    HelloWorld
+    mdbBtn
   }
 };
 </script>
+<style scoped>
+img {
+  filter: brightness(0.3);
+  height: 100vh;
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  z-index: -100;
+}
+h2,
+p {
+  color: #fff;
+}
+.welcome-msg {
+  width: 40%;
+  position: absolute;
+  top: 50vh;
+  left: 50vw;
+  transform: translate(-50%, -70%);
+}
+</style>
