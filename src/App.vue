ee3825936671a04450d973ef25f8f08c5151f7c1
Monday Victor A <vkingmaker1@gmail.com>
2019-10-31 12:32:48 +0100

    chore: setup application to use typescript and MDBVue

---
+++ src/App.vue
@@ -0,0 +1,45 @@
+<template>
+<mdb-container>
+  <mdb-row id="app">
+    <mdb-col id="nav" md="12">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </mdb-col>
+    <router-view class="mx-auto"/>
+  </mdb-row>
+</mdb-container>
+</template>
+
+<script>
+import { mdbContainer, mdbRow, mdbCol } from "mdbvue";
+export default {
+  components: {
+    mdbContainer,
+    mdbRow,
+    mdbCol
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
+}
+</style>
---
+++ src/main.ts
@@ -0,0 +1,14 @@
+import 'bootstrap-css-only/css/bootstrap.min.css';
+import 'mdbvue/build/css/mdb.css';
+import Vue from 'vue';
+import App from './App.vue';
+import router from './router';
+import store from './store';
+
+Vue.config.productionTip = false;
+
+new Vue({
+  router,
+  store,
+  render: h => h(App)
+}).$mount('#app');
